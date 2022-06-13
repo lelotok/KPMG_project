e41d1e781d70938313a4c074342bb08e6bba01bd
lelotok <98822210+lelotok@users.noreply.github.com>
2022-06-13 21:01:54 +0200

    Translation script final

--- translation_copy.py
+++ translation_copy.py
@@ -1,14 +1,13 @@
 import pandas as pd
 from deep_translator import GoogleTranslator
-import config_translation
 
 #Opening file
-df = pd.read_csv(config_translation.PATH_TO_CSV)
+df = pd.read_csv('data/data_03.csv',sep="|",encoding='utf-8')
 
 #Looping through every rows of scaped texts
 for index, rows in df.iterrows():
     # Transform array to string or the translation will give an error
-    text = "".join(df[config_translation.COL_TO_BE_TRANSL][index])
+    text = "".join(df['fr_body'][index])
     to_translate = text
 
     transl = []
@@ -17,11 +16,11 @@ for index, rows in df.iterrows():
         # To prevent the max characters limitations error to stop the script to finish looping throught the entire file
     if len(text) > 5000:
         to_translate = text[:4999]
-        translated = GoogleTranslator(source='auto', target=config_translation.TARGET_LANG).translate(to_translate)
+        translated = GoogleTranslator(source='auto', target='english').translate(to_translate)
         transl.append(translated)
     
     else:
-        translated = GoogleTranslator(source='auto', target=config_translation.TARGET_LANG).translate(to_translate)
+        translated = GoogleTranslator(source='auto', target='english').translate(to_translate)
         transl.append(translated)
 
     # Saving each translations in .txt file
@@ -34,7 +33,7 @@ for index, rows in df.iterrows():
 
 # Create a new column with the translated text in the dataframe
 transl_val = pd.Series(transl)
-df[config_translation.TRANL_COL] = transl_val
+df['transl_engl'] = transl_val
 
 # Save new dataframe in new .csv file
-df.to_csv(config_translation.PATH_TO_NEW_CSV)
+df.to_csv('data/data_translated_02.csv', index=False)
